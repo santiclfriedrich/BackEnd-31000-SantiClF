9dbb0a715d233603d93f0ac9ca625514691d7e1c
santiclfriedrich <santiclear1@hotmail.com>
2022-08-30 02:46:32 -0300

    pushClases || Desafio 30

---
+++ Clase29.Cluster&Escabilidad/parte1/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cluster from 'cluster';
+import os from "os";
+
+const app = express();
+const cpus = os.cpus();
+
+app.get("/", (req, res) => {
+    console.log(`Worker ${process.pid} procesing request `);
+    for (let index = 0; index < 1e9; index++) {}
+    res.send("Todo ok");
+    cluster.worker.kill();
+});
+
+if(cluster.isPrimary){
+    console.log(cpus.length);
+    cpus.map(() => {
+        cluster.fork()
+    });
+
+    cluster.on('exit', worker => {
+        console.log(`Worker ${worker.process.pid} died`);
+
+        cluster.fork();
+    });
+
+} else {
+
+    app.listen(3000, () => {
+        console.log(`Server listening port 3000 - worker: ${process.pid}`);
+    });
+
+}
+
